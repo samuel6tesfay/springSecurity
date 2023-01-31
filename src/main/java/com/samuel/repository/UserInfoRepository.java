977fddf7301810d5bc67e4369204ee85414341ef
samuel6tesfay <samuel6tesfay@gmail.com>
2023-01-31 07:35:31 -0500

    spring security

---
+++ src/main/java/com/samuel/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.samuel.repository;
+
+import com.samuel.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+
+}
